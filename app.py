ad3905a4b15038e3aa78df28256e4a644bc06ae7
LlamaWolfy <87222772+LlamaWolfy@users.noreply.github.com>
2022-01-17 19:47:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Test Instance Deployment</h1><br><br><p>To be tested for GET response prior to deploying to prodcution.</p><br><br><h3>Good luck Jedi!</h3>'
+    return '<h1>Test Instance Deployment</h1><br><br><p>To be tested for GET response prior to deploying to prodcution.</p><br><br><h3>Good luck Jedi scum!</h3>'
