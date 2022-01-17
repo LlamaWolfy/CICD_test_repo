8df4105a6d81833034d1bfd452f67c54b83e8729
LlamaWolfy <87222772+LlamaWolfy@users.noreply.github.com>
2022-01-17 19:37:45 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,6 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '<h1>Test Instance Deployment</h1><br><br><p>To be tested for GET response prior to deploying to prodcution.</p><br><br><h3>Good luck!</h3>'
