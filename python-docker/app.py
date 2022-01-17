ae05b00c5f3b272371cfb5d3aed0a3a2b0abb574
LlamaWolfy <87222772+LlamaWolfy@users.noreply.github.com>
2022-01-17 19:37:55 +0530

    Delete python-docker directory

--- python-docker/app.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return '<h1>Test Instance Deployment</h1><br><br><p>To be tested for GET response prior to deploying to prodcution.</p><br><br><h3>Good luck!</h3>'
